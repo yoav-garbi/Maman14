6306ba1640ec05bb78113b0e4b73e8fc6af71782
yoav-garbi <yoav1garbi@gmail.com>
2025-07-11 14:09:26 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "errors.h"
+#include "func_prototypes.h"
 
 
 int main (int argc, char *argv[])
 {
 	int i;
 	FILE *filePointer;
-	FILE *fileArr[argc-1];
+	FILE **fileArr = malloc(4 * sizeof(FILE));
+	
+	binTree *root;																								/* TEMP */
 	
 	
+	/* take in the arguments into the FILEs array */
 	for (i = 1; i < argc; ++i)
 	{
 		filePointer = fopen(argv[i], "r");
@@ -23,5 +26,27 @@ int main (int argc, char *argv[])
 		fileArr[i-1] = filePointer;
 	}
 	
+	
+	
+	
+	
+	
+	root = makeNode("root", 100);																									/* TEMP */
+	setL(root, makeNode("node1", 200));
+	setR(root, makeNode("node2", 300));
+	
+	printTree(root);
+
+	
+	
+	/* close all files */
+	for (i = 0; i < argc-1; ++i)
+	{
+		fclose(fileArr[i]);
+	}
+	
+	
+	
+	free(fileArr);
 	return 0;
 }
