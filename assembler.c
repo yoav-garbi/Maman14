982e47227724ee994ae78398ea8108f704e650de
yoav-garbi <yoav1garbi@gmail.com>
2025-07-25 16:45:03 +0300

    Update assembler.c
    this version of assembler implements the new funcs of io.c but they are untested

--- assembler.c
+++ assembler.c
@@ -9,40 +9,21 @@ extern opcd opcodeTable[16];
 
 int main (int argc, char *argv[])
 {
-	int i;
-	FILE *filePointer;
-	FILE **fileArr = malloc(4 * sizeof(FILE));
+	/*int i;*/
+	/*FILE *filePointer;*/
+	FILE **fileArr;
 	
 	
-	/* take in the arguments into the FILEs array */
-	for (i = 1; i < argc; ++i)
-	{
-		filePointer = fopen(argv[i], "r");
-		
-		if (checkFileExistence(filePointer) == ERROR)
-		{
-			return ERROR;
-		}
-		
-		fileArr[i-1] = filePointer;
-	}
+	
+	fileArr = getFiles(argc, argv);	/* take in the arguments into the FILEs array */
+	if (fileArr == NULL)
+		return ERROR;
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	/* close all files */
-	for (i = 0; i < argc-1; ++i)
-	{
-		fclose(fileArr[i]);
-	}
-	
-	
-	
+	closeFiles(argc, fileArr);
 	free(fileArr);
 	return 0;
 }
