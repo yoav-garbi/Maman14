40af0db2055d234acbdb0bde3280c7d6ae731c94
yoav-garbi <yoav1garbi@gmail.com>
2025-07-28 20:55:46 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,6 +1,7 @@
 #include "prototypes.h"
 #include "header.h"
 
+int lineCounter;
 
 
 /* check if a file with a given name exists in the directory */
@@ -8,7 +9,7 @@ int check_fileExistence(void *pointer)
 {
 	if (pointer == NULL)
 	{
-		fprintf(stderr, "\nSource file does not exist.\n\n");
+		printf("\nSource file does not exist. (Line %d)\n\n", lineCounter);
 		return ERROR;
 	}
 	
@@ -22,7 +23,7 @@ int check_opcodeName(int index)
 	if (index < num_of_opcodes) /* a command was found */
 		return 0;
 	
-	fprintf(stderr, "\nUnknown command name.\n\n");
+	printf("\nUnknown command name. (Line %d)\n\n", lineCounter);
 	return ERROR;
 }
 
@@ -44,6 +45,42 @@ int check_legalAddressing(int opcodeIndex, int sourceMethod, int destMethod)
 	if (status1 && status2)	/* both methods are legal for this opcode */
 		return 0;
 	
-	fprintf(stderr, "\nIlegal addressing method for this command.\n\n");
+	printf("\nIlegal addressing method for this command. (Line %d)\n\n", lineCounter);
 	return ERROR;
+}
+
+
+
+int check_lineLength(char buffer[])
+{
+	int status = strlen(buffer) <= 82; /* 80 chars + \n + \0 */
+	
+	if (status)
+		return 0; /* this means that the line has legal length */
+	
+	printf("\nLine exceeding the allowed length of 80 chars. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+int check_registerNumber(char name[])
+{
+	if (name[0] == 'r' && (name[1] >= '0' && name[1] <= '7'))
+		return 0; /* register name is correct */
+	
+	printf("\nRegister with this name doesn't exist. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+
+int check_labelLength(char buffer[])
+{
+	int status = strlen(buffer) <= 31; /* 30 chars + \0 */
+	
+	if (status)
+		return 0; /* this means that the label has legal length */
+	
+	printf("\nLabel exceeding the allowed length of 30 chars. (Line %d)\n\n", lineCounter);
+	return ERROR;	
 }
