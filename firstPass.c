4ad8f16ecae143be5fafec834535bba93d6cd411
Tomer40 <tomerohana11@gmail.com>
2025-07-30 20:08:11 +0300

    Update firstPass.c
    עדכון

--- firstPass.c
+++ firstPass.c
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "firstPass.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,16 +9,6 @@
  * Skips all leading spaces and tab characters in a line.
  * Returns a pointer to the first non-whitespace character.
  */
-char *skipWhitespace(char *line);
-
-
-/*
- * Checks if the first word in the line is a label.
- * Returns 1 if a label is found, 0 otherwise.
- */
-int isLabel(const char *ptr);
-
-
 char *skipWhitespace(char *line) {
     while (*line == ' ' || *line == '\t') {
         line++;
@@ -26,8 +16,11 @@ char *skipWhitespace(char *line) {
     return line;
 }
 
-
-int isLabel(const char *ptr,binTree *lableTable) {
+/*
+ * Checks if the first word in the line is a label.
+ * Returns 1 if a label is found, 0 otherwise.
+ */
+int isLabel(const char *ptr) {
     int len = 0;
     while (ptr[len] && !isspace((unsigned char)ptr[len]) && ptr[len] != ':') {
         len++;
@@ -38,33 +31,66 @@ int isLabel(const char *ptr,binTree *lableTable) {
     return 0;
 }
 
-int firstPass(const char *fileName) {
+int firstPass(const char *fileName,binTree *labelTable) {
     FILE *fp;
-    int IC=IC_INIT_VALUE;
-    int DC=DC_INIT_VALUE;
+    int IC = IC_INIT_VALUE;
+    int DC = DC_INIT_VALUE;
     char first_word[MAX_LABEL_LENGTH];
-    int lineNumber=1;
+    int lineNumber = 1;
+    int countError = 0;
     LineDate currentLine;
-    fp=fopen(fileName, "r");
-    /*maybe change for method*/
-    if (fp==NULL) {
+    char *ptr;
+
+    fp = fopen(fileName, "r");
+    /*maybe change to a method */
+    if (fp == NULL) {
         printf("Error opening file %s\n", fileName);
-    }
-    currentLine.lineNumber=lineNumber;
-    while (fgets(currentLine.content, MAX_LINE_LENGTH, fp)!=NULL) {
-        sscanf(currentLine.content, "%s", first_word);
-        if (isLabel(first_word)) {
-            /*if the line starts with a lable then we would add it to the lable table*/
-            /*need to use method to check if lable name is ok(len<30) and doesnt exsist*/
-        }
-        else if (first_word =="entry") {
-            break;
-        }
-        else if (first_word =="external") {
-            /* do instruction 10 from the algo */
-        }
-        /*need to check if first_word is in the op code table */
-        /*need to create data base for the op code commends to save them for later8/
+        return -1;
     }
 
+    while (fgets(currentLine.content, MAX_LINE_LENGTH, fp) != NULL) {
+        currentLine.lineNumber = lineNumber;
+        currentLine.hasError = 0;
+        currentLine.hasLabel = 0;
+        currentLine.error[0] = '\0';
+        currentLine.label[0] = '\0';
+
+        ptr = skipWhitespace(currentLine.content);
+
+        sscanf(ptr, "%s", first_word);
+
+        if (isLabel(ptr)) {
+            /*maybe change to a method */
+            if (strlen(first_word) > MAX_LABEL_LENGTH) {
+                countError++;
+                currentLine.hasError = 1;
+                strcpy(currentLine.error, "label name too long");
+            } else {
+                strcpy(currentLine.label, first_word);
+                currentLine.hasLabel = 1;
+            }
+        }
+
+        else if (strcmp(first_word, ".entry") == 0) {
+            lineNumber++;
+            continue;
+        }
+
+        else if (strcmp(first_word, ".extern") == 0) {
+            lineNumber++;
+
+            continue;
+        }
+
+        /* TODO: check if first_word is in the opcode table */
+        /* TODO: process data, string, mat, etc */
+
+        lineNumber++;
+    }
+    fclose(fp);
+
+    if (countError > 0) {
+        return countError;
+    }
+    return 0;
 }
