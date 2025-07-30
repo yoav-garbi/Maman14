6f5850f16413e6026d8e53b9a109eb086e1e21a0
yoav-garbi <yoav1garbi@gmail.com>
2025-07-30 20:22:04 +0300

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -1,26 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define ERROR -1
-#define buffer_size 100
+#define buffer_size 85
 #define binary_representation_size 9
 #define total_num_of_files(argc) ((argc)*2+2) /* argc*2 because each as file will make a new ob file. +2 because there are an additional two file- ext and ent */
+#define EOF_only_line 1
+#define num_of_opcodes 16
+
+#define CODE 0
+#define DATE 1
+#define MAT 2
+#define EXTERN 3
+#define ENTRY 4
+
+
+extern int lineCounter;
 
 /* the struct that holds the labels is a binary search tree- each node has two brnaches- the smaller is to the left, the bigger is to the right. This is very efficient and allows a time complexity of O(log n) both for adding a node and searching, and a space complexity of O(n) */
 typedef struct binTree
 {
 	char *str;
 	int address;
+	int symbolType;
+	int isExternal: 1;
+	int isEntry: 1;
 	struct binTree *left;
 	struct binTree *right;
 	
 } binTree;
-
-/* struct that will serve as a package for all relevant data for each row, making error handling easier */
-typedef struct {
-    char content[MAX_LINE_LENGTH];
-    int lineNumber;
-    int hasError;
-    
-} LineDate;
-
 
 /* this struct holds necessary information about the opcodes and their permissions. Time complexity for searching an opcode is O(1) because their is a constant amount of opcodes (16). space complexity is O(1) for the same reason */
 typedef struct opcd
@@ -36,12 +45,21 @@ typedef struct opcd
 
 /* io.c */
 FILE **getFiles(int, char *[]);
-int closeFiles(int argc, FILE **fileArr);
+int closeFiles(int argc, FILE **);
+int takeInLine(char [], FILE *);
+int skipNotesAndWhiteLines(char [], FILE *);
+int recognize_opcode(char *);
+
 
 
 
 /* errors.c */
-int checkFileExistence(void*);
+int check_fileExistence(void*);
+int check_opcodeName(int);
+int check_legalAddressing(int, int, int);
+int check_lineLength(char []);
+int check_registerNumber(char []);
+int check_labelLength(char []);
 
 
 
@@ -51,10 +69,11 @@ int base10_to_base2(int, char[]);
 
 
 
+
 /* binTree_funcs.c */
-binTree * makeNode(char *, int);
+binTree * makeNode(char *, int, int, int, int);
 int setL(binTree *, binTree *);
 int setR(binTree *, binTree *);
 int printTree(binTree *);																								/* TEMP */
-int addNode(binTree *, char *, int);
+int addNode(binTree *, char *, int, int, int, int);
 binTree * search(binTree *, char *);
