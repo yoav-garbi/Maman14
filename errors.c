f2f817a7270fcb879281710fb6e1e5ca0501e576
yoav-garbi <yoav1garbi@gmail.com>
2025-07-31 23:52:41 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -4,12 +4,44 @@
 int lineCounter;
 
 
+/* go through a command line and search for all errors that may lie in it */
+int check_lineGeneral(char *line)
+{
+	int status, charsRead;
+	char word[MAX_LINE_LENGTH];
+	
+	/* check if the label is legal */
+	sscanf(line, "%s%n", word, &charsRead);
+	line += charsRead;
+	status = isLabel(word);
+	
+	if (status == 1)																						/* switch 1 to ERROR in isLabel */
+	{
+		lineCounter++;
+		return ERROR;
+	}
+	
+	/* check if command is legal */
+	sscanf(line, "%s%n", word, &charsRead);
+	line += charsRead;
+	
+	status = recognize_opcode(word);
+	if (status == ERROR)
+		return ERROR;
+		
+	/*TODO- add handeling for ilegal addressing and excessive text before/in/after line*/
+	
+	return 0;
+}
+
+
+
 /* check if a file with a given name exists in the directory */
 int check_fileExistence(void *pointer)
 {
 	if (pointer == NULL)
 	{
-		printf("\nSource file does not exist. (Line %d)\n\n", lineCounter);
+		printf("\nSource file does not exist.\n\n");
 		return ERROR;
 	}
 	
@@ -53,9 +85,12 @@ int check_legalAddressing(int opcodeIndex, int sourceMethod, int destMethod)
 
 int check_lineLength(char buffer[])
 {
-	int status = strlen(buffer) <= 82; /* 80 chars + \n + \0 */
+	int len = strlen(buffer);
+	int status = len > 0 && len <= MAX_LINE_LENGTH;
 	
-	if (status)
+	if (len == MAX_LINE_LENGTH && buffer[80] != '\n');	/* this check makes sure that a line that has 81 chars and EOF doesn't go unnoticed */
+	
+	else if (status)
 		return 0; /* this means that the line has legal length */
 	
 	printf("\nLine exceeding the allowed length of 80 chars. (Line %d)\n\n", lineCounter);
@@ -76,7 +111,8 @@ int check_registerNumber(char name[])
 
 int check_labelLength(char buffer[])
 {
-	int status = strlen(buffer) <= 31; /* 30 chars + \0 */
+	int len = strlen(buffer);
+	int status = len > 0 && len <= MAX_LABEL_LENGTH;
 	
 	if (status)
 		return 0; /* this means that the label has legal length */
@@ -84,3 +120,69 @@ int check_labelLength(char buffer[])
 	printf("\nLabel exceeding the allowed length of 30 chars. (Line %d)\n\n", lineCounter);
 	return ERROR;	
 }
+
+
+
+int check_labelName(char *ptr, int len)
+{
+	if (!isalpha(ptr[0]))
+	{
+		printf("\nLabel name starts with non-letter. (Line %d)\n\n", lineCounter);
+		return ERROR;
+	}
+	
+	if (ptr[len] != ':')
+	{
+		printf("\nMissing ':' at the end of label. (Line %d)\n\n", lineCounter);
+		return ERROR;
+	}
+	
+	return 0;
+}
+
+
+
+int check_allocation(void *pointer)
+{
+	if (pointer != NULL)	/* check if memory was allocated succesfuly */
+		return 0;	
+	
+	printf("\nMemory allocation failed.\n\n");
+	return ERROR;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+																																		/* TEMP */
+int isLabel(const char *ptr) {
+    int len = 0;
+    while (ptr[len] && !isspace((unsigned char)ptr[len]) && ptr[len] != ':') {
+        len++;
+    }
+    if (check_labelName((char *)ptr, len) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
