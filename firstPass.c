6809695ee172eb5c28601d521c17e702f3f00813
Tomer40 <tomerohana11@gmail.com>
2025-07-31 03:49:51 +0300

    Update firstPass.c

--- firstPass.c
+++ firstPass.c
@@ -1,8 +1,22 @@
+#include "firstPass.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include "constants.h"
 #include "prototypes.h"
+
+/*
+ * Checks if the word is a data directive (.data, .string, .mat).
+ * Returns 1 if true, 0 otherwise.
+ */
+int isData(const char *word);
+
+/*
+ * Checks if the word is a legal opcode (instruction).
+ * Returns 1 if true, 0 otherwise.
+ * Uses the OPCODES array defined elsewhere.
+ */
+int isInstruction(const char *word);
 
 /*
  * Skips all leading spaces and tab characters in a line.
@@ -16,6 +30,7 @@ char *skipWhitespace(char *line);
  */
 int isLabel(const char *ptr);
 
+char *opCodes[num_of_opcodes] = { "mov", "cmp", "add", "sub", "not", "clr", "lea", "inc","dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop"};
 
 char *skipWhitespace(char *line) {
     while (*line == ' ' || *line == '\t') {
@@ -35,18 +50,75 @@ int isLabel(const char *ptr) {
     return 0;
 }
 
-int firstPass(const char *fileName,binTree *labelTable) {
+int isData(const char *word) {
+    return (strcmp(word, ".data") == 0 ||
+            strcmp(word, ".string") == 0 ||
+            strcmp(word, ".mat") == 0);
+}
+
+int isInstruction(const char *word) {
+    int i;
+    for (i = 0; i < num_of_opcodes; i++) {
+        if (strcmp(word, opCodes[i]) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+int addSymbolToData(binTree **root, char *str, int address) {
+    binTree *found;
+
+    found = search(*root, str);
+    if (found != NULL) {
+        printf("Error symbol '%s' already exists!\n", str);
+        return 0;
+    }
+
+    if (*root == NULL) {
+        *root = makeNode(str, address, DATE, 0, 0);
+        return 1;
+    }
+
+    addNode(*root, str, address, DATE, 0, 0);
+    return 1;
+}
+
+int countDataValues(const char *line) {
+    int count = 0;
+    const char *p = line;
+    while (*p) {
+        if (*p == ',' || *p == '-' || isdigit((unsigned char)*p)) {
+            count++;
+
+            while (*p && (isdigit((unsigned char)*p) || *p == '-' || *p == '+')) p++;
+
+            while (*p && (isspace((unsigned char)*p) || *p == ',')) p++;
+        } else {
+            p++;
+        }
+    }
+    return count;
+}
+
+int countWordsForInstruction(const char *line) {
+    /*TODO:write this method*/
+    return 1;
+}
+
+int firstPass(const char *fileName, binTree **labelTable) {
     FILE *fp;
     int IC = IC_INIT_VALUE;
     int DC = DC_INIT_VALUE;
     char first_word[MAX_LABEL_LENGTH];
+    char label[MAX_LABEL_LENGTH];
     int lineNumber = 1;
     int countError = 0;
     LineDate currentLine;
     char *ptr;
+    char *nextPtr;
 
     fp = fopen(fileName, "r");
-    /*maybe change to a method */
+    /*maybe switch to an ERROR method */
     if (fp == NULL) {
         printf("Error opening file %s\n", fileName);
         return -1;
@@ -61,36 +133,79 @@ int firstPass(const char *fileName,binTree *labelTable) {
 
         ptr = skipWhitespace(currentLine.content);
 
-        sscanf(ptr, "%s", first_word);
-
         if (isLabel(ptr)) {
-            /*maybe change to a method */
-            if (strlen(first_word) > MAX_LABEL_LENGTH) {
+            sscanf(ptr, "%[^:]:", label);
+            currentLine.hasLabel = 1;
+            strcpy(currentLine.label, label);
+
+            nextPtr = strchr(ptr, ':');
+            if (nextPtr != NULL) {
+                nextPtr++;
+                nextPtr = skipWhitespace(nextPtr);
+                sscanf(nextPtr, "%s", first_word);
+            } else {
                 countError++;
                 currentLine.hasError = 1;
-                strcpy(currentLine.error, "label name too long");
-            } else {
-                strcpy(currentLine.label, first_word);
-                currentLine.hasLabel = 1;
+                strcpy(currentLine.error, "Label not followed by command");
+                lineNumber++;
+                continue;
+            }
+        } else {
+            sscanf(ptr, "%s", first_word);
+        }
+
+        if (strcmp(first_word, ".entry") == 0) {
+            lineNumber++;
+            continue;
+        }
+        if (strcmp(first_word, ".extern") == 0) {
+            /*TODO:add to label table */
+            lineNumber++;
+            continue;
+        }
+
+        if (currentLine.hasLabel) {
+            if (isData(first_word)) {
+                addSymbolToData(labelTable, currentLine.label, DC);
+                if (strcmp(first_word, ".data") == 0) {
+                    DC += countDataValues(nextPtr + strlen(first_word));
+                } else if (strcmp(first_word, ".string") == 0) {
+                    char *start = strchr(nextPtr, '\"');
+                    if (start) {
+                        char *end = strchr(start + 1, '\"');
+                        if (end && end > start) {
+                            DC += (end - start - 1) + 1;
+                        }
+                    }
+                } else if (strcmp(first_word, ".mat") == 0) {
+                    /*TODO:write a method for an arry*/
+                }
+            } else if (isInstruction(first_word)) {
+                addNode(*labelTable, currentLine.label, IC, CODE, 0, 0);
+                IC += countWordsForInstruction(nextPtr + strlen(first_word));
+            }
+        } else {
+            if (isData(first_word)) {
+                if (strcmp(first_word, ".data") == 0) {
+                    DC += countDataValues(nextPtr + strlen(first_word));
+                } else if (strcmp(first_word, ".string") == 0) {
+                    char *start = strchr(nextPtr, '\"');
+                    if (start) {
+                        char *end = strchr(start + 1, '\"');
+                        if (end && end > start) {
+                            DC += (end - start - 1) + 1;
+                        }
+                    }
+                } else if (strcmp(first_word, ".mat") == 0) {
+                    /*TODO:write a method for an arry*/
+                }
+            } else if (isInstruction(first_word)) {
+                IC += countWordsForInstruction(nextPtr + strlen(first_word));
             }
         }
-
-        else if (strcmp(first_word, ".entry") == 0) {
-            lineNumber++;
-            continue;
-        }
-
-        else if (strcmp(first_word, ".extern") == 0) {
-            lineNumber++;
-
-            continue;
-        }
-
-        /* TODO: check if first_word is in the opcode table */
-        /* TODO: process data, string, mat, etc */
-
         lineNumber++;
     }
+
     fclose(fp);
 
     if (countError > 0) {
