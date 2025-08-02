4146239ba0e22afd356104b29a623e12fd3d5fc8
yoav-garbi <yoav1garbi@gmail.com>
2025-08-02 11:15:09 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -8,12 +8,12 @@ int main (int argc, char *argv[])
 	/*int i;*/
 	/*FILE *filePointer;*/
 	FILE **fileArr;
+	lineNode **lineArr = calloc(argc - 1, sizeof(lineNode *)); /* an array of pointers to linked lists. each linked list is like a file because it holds all lines */
 	
 	
 	fileArr = getFiles(argc, argv);	/* take in the arguments into the FILEs array */
 	if (fileArr == NULL)
 		return ERROR;
-	
 	
 	
 	closeFiles(argc, fileArr);
