0ec04b28209b02c17ec3aa1079ac5d55dd0084dd
yoav-garbi <yoav1garbi@gmail.com>
2025-08-02 11:28:10 +0300

    Update secondPass.c

--- secondPass.c
+++ secondPass.c
@@ -5,11 +5,11 @@ extern binTree *labelTable;
 
 int secondPass(int argc, FILE **fileArr, lineNode *lineArr[])
 {
-	int fileCount /*, status*/;
+	int fileCount /*, status*/, errorFlag = 0;
 	char /* *line,*/ *character, label[buffer_size], binAddress[binary_representation_size];
 	binTree *node;	
 	
-	
+	/* replace lebels with address */
 	for (fileCount = 0; argc-1; ++fileCount) /* each iteration is one file */
 	{
 		for (lineCounter = 0; lineArr[lineCounter] != NULL; lineCounter++) /* each iteration is one line */
@@ -26,7 +26,10 @@ int secondPass(int argc, FILE **fileArr, lineNode *lineArr[])
 				node = search(labelTable, label);
 				
 				if (check_labelExist(node) == ERROR)
+				{
+					errorFlag = 1;
 					break; /* go to next line */
+				}
 				
 				base10_to_base2_forAddress(node->address, binAddress); /* translate address to binary */
 				
@@ -40,17 +43,18 @@ int secondPass(int argc, FILE **fileArr, lineNode *lineArr[])
 				
 				/* continue scanning from current character for more labels */
 			}
-		
-			
-			/* TODO- if EOF- exit loop*/
-		
-			/* TODO- complete binary coding by replacing the labels with the address */
-		
 		}
 		
 		
-		/* TODO- output files (if no errors) */
+		/* TODO- copy lines to file */
 		
+		
+		/* create extension files (if there were no errors) */
+		if (errorFlag == 1)
+			return ERROR;
+		
+		for (fileCount = 0; argc-1; ++fileCount) /* each iteration is one file */
+			writeOutFiles(fileArr[3*(argc-1) + fileCount], fileArr[3*(argc-1) + fileCount]);
 		
 		
 	}
