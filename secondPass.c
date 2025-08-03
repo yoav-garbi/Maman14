0e314efa6318834ecb14720e872a304a295bbc82
yoav-garbi <yoav1garbi@gmail.com>
2025-08-03 11:40:07 +0300

    Update secondPass.c

--- secondPass.c
+++ secondPass.c
@@ -2,167 +2,80 @@
 
 
 
-/* creates a node and fills it with the data entered (branches are set to NULL) */
-binTree * makeNode(char *str, int address, int type, int external, int entry)
+int secondPass(int argc, char *argv[], FILE **fileArr, lineNode *lineArr[], char **nameArr)
 {
-	binTree *node = malloc(sizeof(binTree));
-	if (node == NULL)
-		return NULL;
+	int fileCount, errorFlag = 0;
+	char *character, label[buffer_size], binAddress[binary_representation_size];
+	binTree *node;
 	
-	node->str = str;
-	node->address = address;
-	node->symbolType = type;
-	node->isExternal = external;
-	node->isEntry = entry;
-	node->left = NULL;
-	node->right = NULL;
-	
-	return node;
-}
-
-/* allows to change the pointer to the left */
-int setL(binTree *node, binTree *left)
-{
-	node->left = left;
-	
-	return 0;
-}
-
-/* allows to change the pointer to the right */
-int setR(binTree *node, binTree *right)
-{
-	node->right = right;
-	
-	return 0;
-}
-
-/* prints the tree (in order) */
-int printTree(binTree *root)																								/* TEMP */
-{
-	if (root == NULL)
-		return 0;
-	
-	printTree(root->left);
-	printf("str: %s\t\t\taddress: %d\t\tsymbolType: %d\t\tisExternal: %d\t\tisEntry: %d\n", root->str, root->address, root->symbolType, root->isExternal, root->isEntry);
-	printTree(root->right);
-	
-	return 0;
-}
-
-
-/* adds a node to the correct place in the tree. time compexity = O(log n) because every level cuts down the number of option by half. space complexity = O(n)- there is no waste of space because nodes are initiallized as needed and not ahead of time. Each node is O(n) because of the string but that is out of out control. In this prohect we will look at a node as a single unit so the space complexity of each node is O(1) 
-The correct place in the tree is determined with the help of strcmp (from <string.h>) that compares 2 string an returns if they are identical or if one has a bigger value (lexicographically) */
-int addNode(binTree **root, char *str, int address, int type, int external, int entry)
-{
-	if (*root == NULL)
-		*root = makeNode(str, address, type, external, entry);
-	
-	else
-		addNodePrivate(*root, str, address, type, external, entry);
-	
-	return 0;
-}
-
-int addNodePrivate(binTree *root, char *str, int address, int type, int external, int entry)
-{
-	int comp;
-	
-	if (root == NULL)
-		return 0;
-	
-	comp = strcmp(str, root->str);
-	
-	
-	if (comp < 0)
+	/* replace lebels with address */
+	for (fileCount = 0; fileCount < argc-1; ++fileCount) /* each iteration is one file */
 	{
-		if (root->left == NULL)
+		for (lineCounter = 0; lineArr[lineCounter] != NULL; lineCounter++) /* each iteration is one line */
 		{
-			setL(root, makeNode(str, address, type, external, entry));
-			return 0;
+			for (character = lineArr[lineCounter]->line; *character != '\n'; character++) /* start of line to end of line, each iteration is one char */
+			{
+				while (*character != ' ' && *character != '\n')
+					character++;
+				
+				if (*character == '\n')
+					break;
+				
+				sscanf(character, "%s", label);
+				node = search(labelTable, label);
+				
+				if (check_labelExist(node) == ERROR)
+				{
+					errorFlag = 1;
+					break; /* go to next line */
+				}
+				
+				base10_to_base2_forAddress(node->address, binAddress); /* translate address to binary */
+				
+				sprintf(character, "%s", binAddress); /* use address to overwrite the label */
+				
+				while (*character != '\0') /* go to end of address */
+					character++;
+				
+				while (*character != ' ' && *character != '\n')	/* delete any remaining parts of the label */
+					*character = ' ';
+				
+				/* continue scanning from current character for more labels */
+			}
 		}
 		
-		addNodePrivate(root->left, str, address, type, external, entry); /* use recursion to decend 1 more level */
-	}
-	
-	else if (comp > 0)
-	{
-		if (root->right == NULL)
+		
+		/* copy lines to file */
+		for (lineCounter = 0; lineArr[lineCounter] != NULL; lineCounter++) /* each iteration is one line */
 		{
-			setR(root, makeNode(str, address, type, external, entry));
-			return 0;
+			
+			/*TODO- create .ob file*/
+				
+			for (character = lineArr[lineCounter]->line; *character != '\0'; character++) /* start of line to end of line, each iteration is one char */
+			{
+				if (*character == ' ')
+					continue;
+				
+				/*fputc(*character, fileArr[2*(argc-1) + fileCount]);															FIX*/
+			}
 		}
-		
-		addNodePrivate(root->right, str, address, type, external, entry); /* use recursion to decend 1 more level */
 	}
 	
-	return 0;
-}
-
-
-
-
-
-/* searches a node (a string) in the tree. time compexity = O(log n) because every level cuts down the number of option by half.
-If the node in question doesnt't exist, the funcion returns NULL  */
-binTree * search(binTree *root, char *str)
-{
-	int comp;
-	
-	if (root == NULL)
-		return NULL;
-	
-	comp = strcmp(str, root->str);
 	
 	
-	if (comp < 0)
-	{
-		if (root->left == NULL)
-			return NULL;
-		
-		return search(root->left, str); /* use recursion to decend 1 more level */
-	}
-	
-	else if (comp > 0)
-	{
-		if (root->right == NULL)
-			return NULL;
-		
-		return search(root->right, str); /* use recursion to decend 1 more level */
-	}
-	
-	return root;
-}
-
-
-
-
-int addLineNode(lineNode **head, char *line, int address)
-{
-	lineNode *newNode = malloc(sizeof(lineNode)), *current;
-
-	if (check_allocation(newNode) == ERROR)
+	/* create extension files (if there were no errors) */
+	if (errorFlag == 1)
 		return ERROR;
-
-	newNode->line = malloc(strlen(line) + 1);
-
-	if (check_allocation(newNode->line) == ERROR)
-		return ERROR;
-
-	strcpy(newNode->line, line);
-	newNode->address = address;
-	newNode->next = NULL;
-
-	if (*head == NULL)
+	
+	/*makeOutFiles(argc, argv, fileArr, nameArr);																REMOVE*/
+	
+	for (fileCount = 0; fileCount < argc-1; ++fileCount) /* each iteration is one file */
 	{
-		*head = newNode;
-		return 0;
+		create_entFile(argc, fileArr, nameArr, fileCount);
+		create_extFile(argc, fileArr, nameArr, fileCount);
+		
+		writeOutFiles(fileArr[3*(argc-1) + fileCount], fileArr[4*(argc-1) + fileCount]);
 	}
-
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-
-	current->next = newNode;
 	
 	return 0;
 }
