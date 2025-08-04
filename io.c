7828f94dd4f4537014dd83aba3e379ab44024a72
yoav-garbi <yoav1garbi@gmail.com>
2025-08-04 11:23:21 +0300

    Update io.c

--- io.c
+++ io.c
@@ -1,268 +1,205 @@
 #include "prototypes.h"
+#include "header.h"
 
 
 
-/* take in the arguments into the FILEs array */
-FILE **getFiles(int argc, char *argv[])
+/* go through a command line and search for all errors that may lie in it */
+int check_lineGeneral(char *line)
 {
-	int i;
-	FILE *filePointer;
-	FILE **fileArr = calloc(total_num_of_files(argc), sizeof(FILE *));
+	int status, charsRead;
+	char word[MAX_LINE_LENGTH];
 	
-	if (check_allocation(fileArr) == ERROR)
-		return NULL;
+	/* check if the label is legal */
+	sscanf(line, "%s%n", word, &charsRead);
+	line += charsRead;
+	status = isLabel(word);
 	
-	/* open .as files */
-	for (i = 0; i < argc - 1; ++i)
+	if (status == 1)																						/* switch 1 to ERROR in isLabel */
 	{
-		filePointer = fopen(argv[i+1], "r");
-		
-		if (check_fileExistence(filePointer) == ERROR)	/* check if the file has opened succesfuly */
-			return NULL;
-		
-		fileArr[i] = filePointer;
+		lineCounter++;
+		return ERROR;
 	}
 	
-	return fileArr;
+	/* check if command is legal */
+	sscanf(line, "%s%n", word, &charsRead);
+	line += charsRead;
+	
+	status = recognize_opcode(word);
+	if (status == ERROR)
+		return ERROR;
+		
+	/*TODO- add handeling for ilegal addressing and excessive text before/in/after line*/
+	
+	return 0;
 }
 
-char **make_nameArr(int argc, char *argv[])
+
+
+/* check if a file with a given pointer exists (opened succesfuly) */
+int check_fileExistence(void *pointer)
 {
-	int i;
-	char **nameArr;
-	
-	/* create an array of strings- each one points to the name of a .as file */
-	nameArr = calloc(argc - 1, sizeof(char *));
-	if (check_allocation(nameArr) == ERROR)
-		return NULL;
-	
-	for (i = 0; i < argc - 1; ++i) /* argc-1 because the 0th index refers to "./assembler" so we need to skip it without going out of bounds */
+	if (pointer == NULL)
 	{
-		nameArr[i] = malloc(buffer_size * sizeof(char));
-		if (check_allocation(nameArr[i]) == ERROR)
-			return NULL;
-		
-		strcpy(nameArr[i], argv[i+1]);	/* argv+1 because the 0th index refers to "./assembler" which is irelevent here, so we need to skip it */
+		printf("\nSource file does not exist.\n\n");
+		return ERROR;
 	}
 	
-	return nameArr;
-}
-
-/* create the output files and store in the FILEs array */
-int create_amFile(int argc, FILE **fileArr, char **nameArr, int i)
-{
-	int len;
-	FILE *filePointer;
-	
-	len = strlen(nameArr[i]);
-	nameArr[i][len-2] = 'a';
-	nameArr[i][len-1] = 'm';
-
-	filePointer = fopen(nameArr[i], "w+");
-
-	if (check_newFileExistence(filePointer) == ERROR)
-		return ERROR;
-
-	fileArr[(argc-1) + i] = filePointer;
-	printf("\t%s\n", nameArr[i]);																						/*TEMP*/
-	
 	return 0;
 }
 
-/* create ob file and store in the FILEs array */
-int create_obFile(int argc, FILE **fileArr, char **nameArr, int i)
+/* check if a file with a given pointer exists (created succesfuly) */
+int check_newFileExistence(void *pointer)
 {
-	int len;
-	FILE *filePointer;
-	
-	len = strlen(nameArr[i]);
-	nameArr[i][len-2] = 'o';
-	nameArr[i][len-1] = 'b';
-
-	filePointer = fopen(nameArr[i], "w+");
-
-	if (check_newFileExistence(filePointer) == ERROR)
-		return ERROR;
-
-	fileArr[2*(argc-1) + i] = filePointer;
-	printf("\t%s\n", nameArr[i]);																						/*TEMP*/
-	
-	return 0;
-}
-
-/* create ent file and store in the FILEs array */
-int create_entFile(int argc, FILE **fileArr, char **nameArr, int i)
-{
-	int len;
-	FILE *filePointer;
-	
-	len = strlen(nameArr[i]);
-	nameArr[i][len-2] = 'e';
-	nameArr[i][len-1] = 'n';
-	nameArr[i][len] = 't';
-	nameArr[i][len+1] = '\0';
-
-	filePointer = fopen(nameArr[i], "w+");
-
-	if (check_newFileExistence(filePointer) == ERROR)
-		return ERROR;
-
-	fileArr[3*(argc-1) + i] = filePointer;
-	printf("\t%s\n", nameArr[i]);																						/*TEMP*/
-	
-	return 0;
-}
-
-/* create ext file and store in the FILEs array */
-int create_extFile(int argc, FILE **fileArr, char **nameArr, int i)
-{
-	int len;
-	FILE *filePointer;
-	
-	len = strlen(nameArr[i]);
-	nameArr[i][len-3] = 'e';
-	nameArr[i][len-2] = 'x';
-	nameArr[i][len-1] = 't';
-	nameArr[i][len] = '\0';
-
-	filePointer = fopen(nameArr[i], "w+");
-
-	if (check_newFileExistence(filePointer) == ERROR)
-		return ERROR;
-
-	fileArr[4*(argc-1) + i] = filePointer;
-	printf("\t%s\n", nameArr[i]);																						/*TEMP*/
-	
-	return 0;
-}
-
-
-
-/* close all files */
-int closeFiles(int argc, FILE **fileArr)
-{
-	int i;
-	
-	for (i = 0; i < total_num_of_files(argc); ++i)
-		if (fileArr[i] != NULL)
-			fclose(fileArr[i]);
-	
-	return 0;
-}
-
-
-
-/* take in the next line of text to the buffer */
-int takeInLine(char buffer[], FILE *source)
-{
-	void *status;
-	
-	status = fgets(buffer, buffer_size, source);
-	lineCounter++;
-	
-	if (status == NULL)	/* fgets returns NULL if the entire input is EOF (an empty line) */
-		return EOF_only_line;
-	
-	if (check_lineLength(buffer) == ERROR)
-		return ERROR;
-	
-	return 0;
-}
-
-
-
-/* read lines until you reach a non-note and non-white-space-only line */
-int skipNotesAndWhiteLines(char buffer[], FILE *source)
-{
-	int i, count, status;
-	char c;
-	
-	do
+	if (pointer == NULL)
 	{
-		status = takeInLine(buffer, source);	/* take line into buffer */
-		if (status == EOF_only_line)	/* if encountered a line that only has EOF then end the task */
-			break;
-		
-		if (status == ERROR)	/* if takeInLine returned error this means that the line is too long, so we can skip it */
-			continue;
-		
-		c = buffer[0];
-		if (c == ';')
-			continue;
-		
-		for (i = 0, count = 0; c != EOF && c != '\0'; ++i)	/* read the line with c and mark count with 1 if encountering a non-white-space (this means a real line) */
-		{
-			c = buffer[i];
-			
-			if (c != ' ' || c != '\t' || c != '\n')
-				count = 1;
-		}
-		
-	} while (count == 0);	/* perform this action as long as count is 0 (as long as the lines are blank) */
+		printf("\nFile failed to be created.\n\n");
+		return ERROR;
+	}
 	
 	return 0;
 }
 
 
-
-int recognize_opcode(char *code)
+int check_fileEntered(int argc)
 {
-	int i, status;
+	if (argc > 1)
+		return 0;
 	
-	for (i = 0; strcmp(code, opcodeTable[i].name); ++i);
+	printf("\nNo files entered.\n\n");
+	return ERROR;
+}
 
-	status = check_opcodeName(i);
-	if(status == 0)	/* command found- return index */
-		return i;
+
+/* check if a command in the source code was mis-written */
+int check_opcodeName(int index)
+{
+	if (index < num_of_opcodes) /* a command was found */
+		return 0;
 	
+	printf("\nUnknown command name. (Line %d)\n\n", lineCounter);
 	return ERROR;
 }
 
 
 
-int writeEnt(FILE *file, binTree *root)
+
+/* check if a given opcode was given an illegal addressing method in the source code */
+int check_legalAddressing(int opcodeIndex, int sourceMethod, int destMethod)
 {
-	char address[address_binary_representation_size+1];	/* +1 is for '\0' */
+	int *permissions1, *permissions2, status1, status2;
+	opcd opcodeTable[num_of_opcodes];
 	
-	if (root == NULL)
+	permissions1 = opcodeTable[opcodeIndex].source;
+	permissions2 = opcodeTable[opcodeIndex].dest;
+	
+	status1 = permissions1[sourceMethod] == 1;
+	status2 = permissions2[destMethod] == 1;
+	
+	if (status1 && status2)	/* both methods are legal for this opcode */
 		return 0;
 	
-	writeEnt(file, root->left);
+	printf("\nIlegal addressing method for this command. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+
+int check_lineLength(char buffer[])
+{
+	int len = strlen(buffer);
+	int status = len > 0 && len <= MAX_LINE_LENGTH;
 	
-	if (root->isEntry)
+	if (len == MAX_LINE_LENGTH && buffer[80] != '\n');	/* this check makes sure that a line that has 81 chars and EOF doesn't go unnoticed */
+	
+	else if (status)
+		return 0; /* this means that the line has legal length */
+	
+	printf("\nLine exceeding the allowed length of 80 chars. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+int check_registerNumber(char name[])
+{
+	if (name[0] == 'r' && (name[1] >= '0' && name[1] <= '7'))
+		return 0; /* register name is correct */
+	
+	printf("\nRegister with this name doesn't exist. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+
+int check_labelLength(char buffer[])
+{
+	int len = strlen(buffer);
+	int status = len > 0 && len <= MAX_LABEL_LENGTH;
+	
+	if (status)
+		return 0; /* this means that the label has legal length */
+	
+	printf("\nLabel exceeding the allowed length of 30 chars. (Line %d)\n\n", lineCounter);
+	return ERROR;	
+}
+
+
+
+int check_labelName(char *ptr, int len)
+{
+	if (!isalpha(ptr[0]))
 	{
-		base10_to_base2_forAddress(root->address, address);
-	
-		fprintf(file, "%s\t\t", root->str);
-		base2_to_base4_strToFile(address, file);
-		fprintf(file, "\n");
+		printf("\nLabel name starts with non-letter. (Line %d)\n\n", lineCounter);
+		return ERROR;
 	}
 	
-	writeEnt(file, root->right);
+	if (ptr[len] != ':')
+	{
+		printf("\nMissing ':' at the end of label. (Line %d)\n\n", lineCounter);
+		return ERROR;
+	}
 	
 	return 0;
 }
 
 
-int writeExt(FILE *file, binTree *root)
+
+int check_allocation(void *pointer)
 {
-	char address[address_binary_representation_size+1];	/* +1 is for '\0' */
+	if (pointer != NULL)	/* check if memory was allocated succesfuly */
+		return 0;	
 	
-	if (root == NULL)
+	printf("\nMemory allocation failed.\n\n");
+	return ERROR;
+}
+
+
+
+int check_labelExist(binTree *node)
+{
+	if (node != NULL)	/* label was found in the label table */
 		return 0;
 	
-	writeExt(file, root->left);
-	
-	if (root->isExternal)
-	{
-		base10_to_base2_forAddress(root->address, address);
-			
-		fprintf(file, "%s\t\t", root->str);
-		base2_to_base4_strToFile(address, file);
-		fprintf(file, "\n");
-	}
-	
-	writeExt(file, root->right);
-	
-	return 0;
+	printf("\nLabel not defined in the assembly file. (Line %d)\n\n", lineCounter);
+	return ERROR;
+}
+
+
+
+
+
+
+
+
+
+
+																																		/* TEMP- this is Tomer's */
+int isLabel(const char *ptr) {
+    int len = 0;
+    while (ptr[len] && !isspace((unsigned char)ptr[len]) && ptr[len] != ':') {
+        len++;
+    }
+    if (check_labelName((char *)ptr, len) == 0) {
+        return 1;
+    }
+    return 0;
 }
