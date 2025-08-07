51735e5af1ee437923fb4c31b068442214a9790e
yoav-garbi <yoav1garbi@gmail.com>
2025-08-07 10:45:12 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -6,13 +6,13 @@ int *dcArr;
 int lineCounter;
 int fileCounter;
 lineNode **entryLineArr;
-
+lineNode **externLineArr;
 
 
 
 int main (int argc, char *argv[])
 {
-	int numFiles = argc-1;
+	int numFiles = argc-1, i, errorFlag = 0, res;
 	lineNode **lineArr = NULL;
 	char **nameArr =  NULL;
 	FILE **fileArr = NULL;
@@ -20,11 +20,15 @@ int main (int argc, char *argv[])
 	icArr = NULL;
 	dcArr = NULL;
 	entryLineArr = NULL;
+	externLineArr = NULL;
 	
 	
 	/* 1) check that source file/s were entered */
 	if (check_fileEntered(argc) == ERROR)
 		goto cleanUp;
+	
+	
+	
 	
 	/* 2) initialize relevant structs and arrays */
 	if (initializeLabelTables(argc) == ERROR) /* initialize labelTables array */
@@ -39,9 +43,15 @@ int main (int argc, char *argv[])
 	if (check_allocation(icArr) == ERROR || check_allocation(dcArr) == ERROR)
 		goto cleanUp;
 	
-	entryLineArr = calloc(numFiles, sizeof(lineNode *)); /* initialize lineArr- lineArr is an array of pointers to linked lists. each linked list is like a file because it holds all lines */
+	entryLineArr = calloc(numFiles, sizeof(lineNode *)); /* initialize entryLineArr- each list holds all entry-d labels from one file */
 	if (check_allocation(entryLineArr) == ERROR)
 		goto cleanUp;
+	
+	externLineArr = calloc(numFiles, sizeof(lineNode *)); /* initialize externLineArr- each list holds all raw lines (from one file) that used an external label */
+	if (check_allocation(externLineArr) == ERROR)
+		goto cleanUp;
+	
+	
 	
 	
 	/* 3) open .as files (store in fileArr) */
@@ -49,30 +59,34 @@ int main (int argc, char *argv[])
 	if (fileArr == NULL)
 		goto cleanUp;
 	
+	
 	/* 4) build nameArr- an array of strings, each one is a name of a .as file */
 	nameArr = make_nameArr(argc, argv);
 	if (nameArr == NULL)
 		goto cleanUp;
-
-
-	/* first pass here */
-	int i;
-	int firstPassError = 0;
-	for (i = 0; i < numFiles; i++) {
-    		int res = firstPass(i);
-    		if (res != 0) {
-        		printf("Error in first pass of file %s (%d errors found)\n", nameArr[i], res);
-        		firstPassError = 1;
-    		}
-	}
-	if (firstPassError) {
-    		printf("Errors were found in the first pass. Compilation stopped\n");
-    		goto cleanUp;
-	}
-
 	
 
-	/* second pass */
+
+	
+	/* 5) first pass */
+	for (i = 0; i < numFiles; i++)
+	{
+    		res = firstPass(i);
+    		if (res != 0) {
+        		printf("Error in first pass of file %s (%d errors found)\n", nameArr[i], res);
+        		errorFlag = 1;
+    		}
+	}
+	
+	if (errorFlag)
+	{
+    		printf("\nErrors were found in the first pass. Compilation terminated\n");
+    		goto cleanUp;
+	}
+	
+	
+	
+	/* 6) second pass */
 	if (secondPass(argc, argv, fileArr, lineArr, nameArr) == ERROR)
 		goto cleanUp;
 	
@@ -90,6 +104,7 @@ int main (int argc, char *argv[])
 	free(icArr);
 	free(dcArr);
 	freeListArr(&entryLineArr, argc-1);
+	freeListArr(&externLineArr, argc-1);
 	
 	return 0;
 }
