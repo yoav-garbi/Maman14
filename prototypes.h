37d981713f38aa59aab2e490b443f9abc888ae72
yoav-garbi <yoav1garbi@gmail.com>
2025-08-09 17:38:37 +0300

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -11,9 +11,28 @@
 #define total_num_of_files(argc) ((argc-1)*5) /* argc-1 because the 0th index refers to "./assembler" which is irrelevent here. *4 because each .as file (1) will make a .am file (2), .ob file (3), a .ext file (4) and a .ent file (5) */
 #define EOF_only_line 1
 #define num_of_opcodes 16
+#define max_immediate_int 127
+#define min_immediate_int -128
 
 #define CODE 0
 #define DATA 1
+
+#define wordIsLABEL 0
+#define wordIsCOMMAND 1
+#define wordIsDATA 2
+#define wordIsSTRING 3
+#define wordIsMAT 4
+#define wordIsENTRY 5
+#define wordIsEXTERN 6
+#define EMPTY_LINE 7
+
+#define ADDR_IMMEDIATE 0 /* #number */
+#define ADDR_DIRECT 1 /* LABEL */
+#define ADDR_INDEX 2 /* mat[][] */
+#define ADDR_REGISTER 3 /* r0-r7 */
+
+
+
 
 /* the struct that holds the labels is a binary search tree- each node has two brnaches- the smaller is to the left, the bigger is to the right. This is very efficient and allows a time complexity of O(log n) both for adding a node and searching, and a space complexity of O(n) */
 typedef struct binTree
@@ -68,6 +87,10 @@ typedef struct {
 } operands;
 
 
+
+extern lineNode **lineArr;
+extern char **nameArr;
+extern FILE **fileArr;
 extern int lineCounter;
 extern int fileCounter;
 extern binTree **labelTable;
@@ -77,10 +100,7 @@ extern int *dcArr;
 extern lineNode **entryLineArr;
 extern lineNode **externLineArr;
 
-extern lineNode **lineArr;
-extern  char **nameArr;
-extern  FILE *fileArr;
-/* TODO:yoav these above should be there no? */
+
 
 /* io.c */
 FILE **getFiles(int, char *[]);
@@ -101,12 +121,11 @@ int writeExt(FILE *, binTree *);
 
 
 /* errors.c */
-int check_lineGeneral(char *, int, int);
+int check_lineGeneral(char *);
 int check_fileExistence(void*);
 int check_newFileExistence(void *);
 int check_fileEntered(int);
 int check_opcodeName(int);
-int check_legalAddressing(int, int, int);
 int check_lineLength(char []);
 int check_registerNumber(char []);
 int check_labelName(char *);
@@ -116,6 +135,15 @@ int check_labelExist_or_legalExternalUse(binTree *, char *, lineNode *, int);
 int check_entryWithLocalDefinition(binTree *, char *);
 int check_labelDuplicate(char *);
 int check_isExternalLabelDefinedInOtherFile(char *, int);
+int check_garbageTextBeforeLine(char *, int *, int *);
+int check_garbageTextAndClassifyWord(char *, int, int *, int *);
+int check_scanOperand(char **, int *, char *);
+int check_dataValues(char **, int *);
+int check_matValues(char **, int);
+int check_stringData(char **);
+int check_commandOperands(char **, char *);
+
+
 
 
 
@@ -126,6 +154,16 @@ int base10_to_base2(int, char[]);
 int base10_to_base2_forAddress(int, char[]);
 int copyFile(FILE *, FILE *);
 char *strDuplicate(char *);
+char *skipWhiteSpace(char *);
+int isEndOfLine_or_whiteSpaceOnly(char *);
+char *skipWhiteSpace(char *);
+int isEndOfLine_or_whiteSpaceOnly(char *);
+int isRequiredComma(char **);
+int scanLabel(char **, char *);
+int scanInt(char **, int *);
+int scanString(char **, char *);
+
+
 
 
 
@@ -153,6 +191,9 @@ int addLineNode(lineNode **, char *, int, int);
 int printList(lineNode *);																								/* TEMP */
 int freeList(lineNode **);
 int freeListArr(lineNode ***, int);
+int isAlreadyEntry(char *);
+int isAlreadyExtern(char *);
+
 
 int freeNameArr(char ***, int);
 
